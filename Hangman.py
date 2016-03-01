febff8c04412e0872b41baae246b2e0d0c3b5d0f
Cody Grinstead <yodacodyone@gmail.com>
2016-03-01 16:07:42 -0700

    final work

--- Hangman.py
+++ Hangman.py
@@ -1,7 +1,9 @@
 #Cody Grinstead
 #Python Hangman
-#Last updated 2/5/16
+#Last updated 2/9/16
 import random
+import os
+
 
 def currentHangman(life):
 	if(life==8):
@@ -22,18 +24,8 @@ def currentHangman(life):
 		print("   _____",'\n',"  |    |",'\n',"__O__  |",'\n',"  |    |",'\n'," _|    |",'\n',"|      |",'\n',"       |",'\n',"_______|")
 	else:
 		print("   _____",'\n',"  |    |",'\n',"__O__  |",'\n',"  |    |",'\n'," _|_   |",'\n',"|   |  |",'\n',"       |",'\n',"_______|")
-		
 
-#get word for a list x
-#ask user for guess x
-#check to see if letter & hasnt been guessed x
-#if eligable letter check with work x
-#if correct guess switch blank spaces with letter x
-#if wrong update life count
-#print current guessed word with current hangman life
-#reapeat till life is 0 or word is guessed
-#ask if user wants to play again
-
+#difficulty lists
 wordListEasy=['fox','cat','dog','mouse','ant','desk','chair','man','woman','phone','sun','ghost',
 'flower','banana','book','light','snake','apple','socks','smile']
 wordlistNormal=['horse','door','song','trip','backbone','bomb','round','treasure','garbage','park',
@@ -41,19 +33,44 @@ wordlistNormal=['horse','door','song','trip','backbone','bomb','round','treasure
 wordListHard=['jazz','jinx','blizzard','quiz','house','buzzer','cozy','rose','daisy','dinner','important',
 'baggage','password','newsletter','bookend','pharmacist','catalog','vegetarian','neighborhood','vitamin']
 
+def isOneChar(guess):
+	if(len(guess)>1):
+		return False
+	elif(len(guess)==0):
+		return False
+	else:
+		return True
+
+def isLetter(guess):
+	isaLetter=True
+	notletters=['1','2','3','4','5','6','7','8','9','0',' ','`','~','!',
+	'@','#','$','%','^','&','*','(',')','_','+','-','=','}','{',']','[',
+	';',':','/','?','.','>',',','<',',',"'",'"']
+	if guess in notletters:
+			isaLetter=False
+	return isaLetter	
+		
+def isNewGuess(guess,guessedletters):#curret is current guess, already is string of already guessed
+	isaNewGuess=True
+	if(guess in guessedletters):
+		isaNewGuess=False
+	return isaNewGuess
+	
 def game(difficulty):
 	life=8
 	guessedcorrectly=0
 	guessedletters=" "
+	guessedlist=[]
 	notletters='1234567890 `~!@#$%^&*()_+-=}{][;:/?.>,<'
-	currentword=difficulty[random.randint(0,9)]
+	currentword=difficulty[random.randint(0,19)]
 	wordlen=len(currentword)
 	currentwordline=''
 	wordlinelist=[]#making a list of stings that can be edited
 	for n in range(0,wordlen):
 		wordlinelist+='_'
 		currentwordline+=wordlinelist[n]
-	while(life>0 and guessedcorrectly<wordlen):		
+	while(life>0 and guessedcorrectly<wordlen):	
+		os.system('cls' if os.name == 'nt' else 'clear')#From stackover flow	
 		isletterword= False 
 		currentHangman(life)
 		print(guessedletters,"\n",currentwordline)
@@ -61,21 +78,20 @@ def game(difficulty):
 		guessl=input("Enter a letter to guess\n")
 		guess=guessl.lower()
 		#making sure guessed letter is a letter and can be used as such
-		for n in range(0,len(guessedletters)): 
-			if(guess==guessedletters[n]):
+		while(isOneChar(guess)==False):
+			print("You can only guess one letter at a time")
+			guessl=input("Enter a letter you have not guessed\n")
+			guess=guessl.lower()
+		while(isLetter(guess)==False):
+			print("You can only enter letters")
+			guessl=input("Enter a letter you have not guessed\n")
+			guess=guessl.lower()
+		while(isNewGuess(guess,guessedlist)==False):
 				print("You have already guessed that letter")
 				guessl=input("Enter a letter you have not guessed\n")
-				guess=guessl.lower()
-			if(len(guess)>1):
-				print("You can only guess one letter at a time")
-				guessl=input("Enter a letter you have not guessed\n")
-				guess=guessl.lower()
+				guess=guessl.lower()	
 		guessedletters+=guess #so user can see current guessed letters
-		for n in range(0,len(notletters)):
-			if(guess==notletters[n]):
-				print("This is not a letter")
-				guessl=input("Enter a letter you have not guessed\n")
-				guess=guessl.lower()		
+		guessedlist+=guess
 		for n in range(0,wordlen):
 			if(guess==currentword[n]):
 				isletterword= True
@@ -87,6 +103,7 @@ def game(difficulty):
 		for n in range(0,wordlen):
 			currentwordline+=wordlinelist[n]
 	if(life>0):
+		os.system('cls' if os.name == 'nt' else 'clear')#From stackover flow
 		print("You correctly guessed the word ", currentword,"!")
 		playagain=input("Would you like to play again? (y/n)  ")
 		if(playagain=='y'):
@@ -95,6 +112,7 @@ def game(difficulty):
 			print("Good bye")
 			return False
 	else:
+		os.system('cls' if os.name == 'nt' else 'clear')#From Stackover flow
 		currentHangman(0)
 		print("Sorry the word was: ",currentword)
 		playagain=input("Would you like to play again? (y/n)  ")
